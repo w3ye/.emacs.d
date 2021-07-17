2bb26181e7fc1a38116240b56c915a0523338e8d
Benny <w3ye@pm.me>
2021-07-17 15:54:12 -0400

    initialized evil, installed general and hydra

--- lisp/global-keybinds.el
+++ lisp/global-keybinds.el
@@ -1,3 +1,28 @@
-(global-set-key (kbd "C-M-j") 'counsel-switch-buffer)
+;; keymapping plugin
+(use-package general)
+
+;; Install hydra
+(use-package hydra)
+
+;; vim mode
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybindings nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  ;; Move the cursor to the warped line instead of jumping to the next line
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line))
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+
+(general-define-key
+ "C-M-j" 'counsel-switch-buffer)
 
 (provide 'global-keybinds)
