c62487b012d453a2d25f6d59a9c16b4b655ca489
Benny <w3ye@pm.me>
2021-07-17 20:17:42 -0400

    Revert "Install company"
    This reverts commit 9f64316e2f3fa58f6a36fc2b8bd5ba1a90f1a365.

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -76,4 +76,24 @@
 ;; NOTE: Make sure to configure the Github token before using - Magit Forge
 (use-package forge)
 
+;; Install company (auto-complete)
+(use-package company
+  :config
+  (setq company-dabbrev-code-everywhere t
+	company-dabbrev-code-modes t
+	company-dabbrev-code-other-buffers 'all
+	company-dabbrev-downcase nil
+	company-dabbrev-ignore-case t
+	company-dabbrev-other-buffers 'all
+	company-require-match nil
+	company-minimum-prefix-length 2
+	company-show-numbers t
+	company-tooltip-limit 20
+	company-idle-delay 0
+	company-echo-delay 0
+	company-tooltip-offset-display 'scrollbar
+	company-begin-commands '(self-insert-command))
+  (push '(company-semantic :with company-yasnippet) company-backends)
+  :hook ((after-init . global-company-mode)))
+
 (provide 'init-packages)
