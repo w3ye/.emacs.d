6ba314207ebb091a35b8f9250213feeb80b8462b
William Ye <w3ye@pm.me>
2021-07-20 00:27:34 -0400

    change js-mode to js2-mode and added better syntax highlights

--- lisp/lang-settings.el
+++ lisp/lang-settings.el
@@ -4,6 +4,11 @@
 
 ;; set javascript indent to 2
 (setq js-indent-level 2)
+(use-package js2-mode)
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(custom-set-faces
+ '(js2-object-property ((t (:inherit font-lock-variable-name-face)))))
+
 
 (provide 'lang-settings)
 ;;; lang-settings.el ends here
