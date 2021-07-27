a7abbcd11f66a93e1b70280b39bc97faa4bb461f
William Ye <w3ye@pm.me>
2021-07-26 23:42:27 -0400

    Set term and compilation mode color

--- lisp/init-startup.el
+++ lisp/init-startup.el
@@ -11,10 +11,16 @@
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (set-fringe-mode 10)
+
+;; (global-visual-fill-column-mode 1)
+;; (global-visual-line-mode 1)
+
 ;; have column numbers
 (column-number-mode 1)
-(global-visual-line-mode 1)
+;; auto bracket
 (electric-pair-mode 1)
+;; show braket pairs
+(show-paren-mode 1)
 ;; Saves auto saves file to ~/.saves
 (setq backup-directory-alist `(("." . "~/.saves")))
 (setq version-control t     ;; Use version numbers for backups.
@@ -28,5 +34,14 @@
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
 ;; Authentication for github - magit forge
 (setq auth-sources '("~/.authinfo"))
+
+;; Compilation mode color
+(require 'ansi-color)
+(defun colorize-compilation-buffer ()
+  (toggle-read-only)
+  (ansi-color-apply-on-region compilation-filter-start (point))
+  (toggle-read-only))
+(add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
+
 (provide 'init-startup)
 ;;; init-startup.el ends here
--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -30,5 +30,12 @@
 	centaur-tabs-set-bar 'over
 	centaur-tabs-set-modified-marker t
 	centaur-tabs-cycle-scope 'tabs))
+(use-package xterm-color)
+(setq compilation-environment '("TERM=xterm-256color"))
+(defun my/advice-compilation-filter (f proc string)
+  (funcall f proc (xterm-color-filter string)))
+(advice-add 'compilation-filter :around #'my/advice-compilation-filter)
+
+
 (provide 'init-ui)
 ;;; init-ui.el ends here
